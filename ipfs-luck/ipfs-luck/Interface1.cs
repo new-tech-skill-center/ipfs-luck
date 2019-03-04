b3845c8f58827c387c82abcccfc5ca64e38707a1
oneplus-ntsc <48100903+oneplus-ntsc@users.noreply.github.com>
2019-03-04 09:24:52 -0800

    Add IPFS and JSON packages, add example interface/class.

---
+++ ipfs-luck/ipfs-luck/Interface1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipfs_luck 
+{
+    interface MyListener
+    {
+        void onReceive(byte[] input);
+        void onReceive(string v);
+    }
+    class b : MyListener {
+
+        public void a(MyListener input)
+        {
+            input.onReceive("mypacket");
+        }
+
+        public void onReceive(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void onReceive(byte[] input)
+        {
+            throw new NotImplementedException();
+        }
+        public void run()
+        {
+            for(;;)
+            {
+                
+            }
+        }
+    }
+}
